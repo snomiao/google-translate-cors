9bda29e83afe9681c867f138b5bf1bde6b06edf0
snomiao <snomiao@gmail.com>
2022-09-24 15:01:57 +0800

    feat(init):

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import { createProxyMiddleware }from 'http-proxy-middleware';
+
+const apiProxy = createProxyMiddleware({
+  target: "https://translate.google.com",
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api?url=https:/translate.google.com/': '/', // remove base path
+  },
+  changeOrigin: true,
+  onProxyRes: (proxyRes) => {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*'; // add new header to response
+  }
+});
+
+// Expose the proxy on the "/api/*" endpoint.
+export default function (req, res) {
+  return apiProxy(req, res);
+};
