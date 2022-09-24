55e03500fdabf511cd32c4279e27c7c30e48d01e
snomiao <snomiao@gmail.com>
2022-09-24 15:29:00 +0800

    docs(): regexp

---
+++ README.md
@@ -0,0 +1,4 @@
+# google translate cors service
+
+Demo:
+https://google-translate-cors.vercel.app/api?url=https://translate.google.com/
--- api/index.js
+++ api/index.js
@@ -7,18 +7,14 @@ const apiProxy = createProxyMiddleware({
     "^/api\\?url=https?://translate.google.com": "", // remove base path
   },
   changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    // proxyReq.req.url;
-  },
   onProxyRes: (proxyRes, req, res) => {
     proxyRes.headers["Access-Control-Allow-Origin"] = "*"; // add new header to response
   },
 });
 
-// Expose the proxy on the "/api/*" endpoint.
+// Expose the proxy on the "/api" endpoint.
 export default function (req, res) {
   req.url = "/api?url=" + req.query.url;
   const r = apiProxy(req, res);
-  console.error(req);
   return r;
 }
